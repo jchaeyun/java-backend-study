4bb8da8477cfd1815dc4988597ac3ae6e4c982b2
jchaeyun <jocheyoon@gmail.com>
2026-05-10 20:22:32 +0900

    docs: #1 #2 JPA 영속성 컨텍스트 이론,실습 정리 완료

---
+++ week-01-jpa/practice/java-basic/src/main/java/com/example/java_basic/D1_VintageItem.java
@@ -0,0 +1,33 @@
+package com.example.java_basic;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class D1_VintageItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int price;
+
+    public D1_VintageItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
