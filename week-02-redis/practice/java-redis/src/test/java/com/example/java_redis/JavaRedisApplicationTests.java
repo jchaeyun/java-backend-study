d75651c88e129c3d050e831955bb0db011809926
jchaeyun <jocheyoon@gmail.com>
2026-05-18 13:29:24 +0900

    chore: week2 폴더와 파일 추가

---
+++ week-02-redis/practice/java-redis/src/test/java/com/example/java_redis/JavaRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
